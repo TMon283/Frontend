2abf7c399ba407d42687ef54fb744fbd720efe87
TMon283 <lemonboy2k6@gmail.com>
2025-02-26 16:59:19 +0700

    Update homework9-ss16.js

--- js/ss16/Bài tập/homework9-ss16.js
+++ js/ss16/Bài tập/homework9-ss16.js
@@ -1,3 +1,3 @@
 let celsius = Number(prompt("Nhập độ C: "));
 let fahrenheit = ((celsius * 9/5) + 32);
-document.write("<b>",celsius, "°C = ",fahrenheit, "°F</b>")
+console.log("<b>"+celsius+ "°C = "+fahrenheit+ "°F</b>");
