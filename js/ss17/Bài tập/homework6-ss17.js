05b6110b1db171ceb118b03fbc7fdfd246cd0a6e
TMon283 <lemonboy2k6@gmail.com>
2025-02-27 17:04:58 +0700

    Update homework6-ss17.js

--- js/ss17/Bài tập/homework6-ss17.js
+++ js/ss17/Bài tập/homework6-ss17.js
@@ -5,8 +5,8 @@ console.log(`   + Chuyển từ "Hello" thành `+Number("Hello")); //Nan
 
 console.log(`- Chuyển đổi số thành chuỗi: `);
 let number1 = 55;
-console.log(`   + Chuyển từ 55 thành ` +number1.toString());
+console.log(`   + Chuyển từ 55 thành ` +number1.toString()); // "55"
 let number2 = -10;
-console.log(`   + Chuyển từ -10 thành ` +number2.toString());
+console.log(`   + Chuyển từ -10 thành ` +number2.toString()); // "-10"
 let number3 = 0;
-console.log(`   + Chuyển từ -10 thành ` +number3.toString());
+console.log(`   + Chuyển từ 0 thành ` +number3.toString()); // "0"
