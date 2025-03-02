288ede7bcc56836338764ba0c368e289448e9194
TMon283 <lemonboy2k6@gmail.com>
2025-03-02 09:14:25 +0700

    Update homework5-ss18.js

--- js/ss18/homework5-ss18.js
+++ js/ss18/homework5-ss18.js
@@ -1,5 +1,5 @@
 let year = +prompt("Nhập số năm kinh nghiệm của nhân viên: ");
-if (year < 1) {
+if (year < 1 && year > 0) {
     console.log("Mới vào nghề");
 } else if (year >= 1 && year <= 3) {
     console.log("Nhân viên có kinh nghiệm");
@@ -7,4 +7,6 @@ if (year < 1) {
     console.log("Chuyên viên");
 } else if (year > 6) {
     console.log("Quản lý");
+} else {
+    console.log("Số năm nhập không hợp lệ");
 }
