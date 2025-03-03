1f709bbcc9ae2cb221451e18b27f4300a6ff245f
TMon283 <lemonboy2k6@gmail.com>
2025-03-03 16:18:57 +0700

    Update homework8-ss19.js

--- js/ss19/Bài tập/homework8-ss19.js
+++ js/ss19/Bài tập/homework8-ss19.js
@@ -1,12 +1,16 @@
 let month = +prompt("Mời bạn nhập tháng: ");
-if (month === 1 || month === 2 || month === 3) {
-    alert("Mùa xuân");
-} else if (month === 4 || month === 5 || month === 6) {
-    alert("Mùa hạ");
-} else if (month === 7 || month === 8 || month === 9) {
-    alert("Mùa thu");
-} else if (month === 10 || month === 11 || month === 12) {
-    alert("Mùa đông");
+if (Number.isInteger(month)){
+    if (month === 1 || month === 2 || month === 3) {
+        alert("Mùa xuân");
+    } else if (month === 4 || month === 5 || month === 6) {
+        alert("Mùa hạ");
+    } else if (month === 7 || month === 8 || month === 9) {
+        alert("Mùa thu");
+    } else if (month === 10 || month === 11 || month === 12) {
+        alert("Mùa đông");
+    } else {
+        alert("Tháng không hợp lệ");
+    }
 } else {
-    alert("Tháng không hợp lệ");
+    alert("Dữ liêu nhập vào không phải số");
 }
